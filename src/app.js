e730dc5ba98662b5058aa1f06a5654b98710cfea
ArnaudD <arnaud.dumoncel@gmail.com>
2023-04-10 21:45:05 +0200

    connexion mongodb

--- src/app.js
+++ src/app.js
@@ -1,6 +1,24 @@
-import express from 'express';
+import express from 'express'
+import mongoose from 'mongoose'
+import * as dotenv from 'dotenv'
 
 const app = express()
+dotenv.config()
+mongoose.connect(`mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_SECRET}@${process.env.MONGODB_CLUSTER}/?retryWrites=true&w=majority`, 
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+)
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(() => console.error('Connection to MongoDB failed'))
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization')
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS')
+  next()
+})
 
 app.use((req, res, next) => {
   console.log('Requête reçue')
