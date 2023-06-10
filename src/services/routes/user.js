cc2c10d2789ba04f6de9f92e8cd2ee8eeb70967a
ArnaudD <arnaud.dumoncel@gmail.com>
2023-06-10 19:26:31 +0200

    auth + request updateUser

--- src/services/routes/user.js
+++ src/services/routes/user.js
@@ -1,10 +1,12 @@
 import express from 'express'
 import * as userController from '../controllers/user.js'
+import { auth } from '../../middleware/auth.js'
 
 const router = express.Router()
 
 router.post('/signup', userController.signup)
 router.post('/login', userController.login)
 router.get('/:id', userController.getUser)
+router.post('/:id', auth, userController.updateUser)
 
 export default router
